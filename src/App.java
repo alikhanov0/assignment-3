2f8a401199d2655b790eb0fba1b6fb41358ab63d
alikhanov <alikhanov8608@gmail.com>
2026-01-24 12:55:42 +0500

    main file changes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,53 @@
+import models.Dorm;
+import models.Room;
+import models.Student;
+import service.DormService;
+import service.RoomService;
+import service.StudentService;
+
+import java.sql.Date;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        StudentService service = new StudentService();
+        DormService dormService = new DormService();
+        RoomService roomService = new RoomService();
+
+        Dorm d1 = new Dorm(3, "Dorm A", "Abay street 10", 300);
+        // dormService.addDorm(d1);
+
+        Student s1 = new Student(
+                9,
+                "Yerbol",
+                "Alikhan",
+                'M',
+                Date.valueOf("2018-09-01"),
+                "alikhanov@gmail.com",
+                "87058751277");
+
+        // service.addStudent(s1);
+
+        Room r = new Room(6, 2, "201", 1, "Double");
+        // roomService.addRoom(r);
+
+        System.out.println("Dorms:");
+        for (Dorm d : dormService.getAll()) {
+            System.out.println(d.getDormId() + " " + d.getDormName());
+        }
+
+        System.out.println("All students:");
+        for (Student s : service.getAll()) {
+            System.out.println(
+                    s.getStudentId() + " " +
+                            s.getFirstName() + " " +
+                            s.getLastName() + " " +
+                            s.getEmail());
+        }
+
+        System.out.println("Rooms:");
+        for (Room x : roomService.getAll()) {
+            System.out.println(x.getRoomId() + " Dorm=" + x.getDormId() + " " + x.getRoomNumber());
+        }
     }
 }
